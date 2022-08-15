1560dbb5418877085ae7536d2a039b064668ddce
CruzRazeto <cruzrazeto@gmail.com>
2022-08-15 19:05:55 -0400

    Add Language Bean and interceptor

--- src/main/java/com/bolsedeideasspringboot/jpa/app/MvcConfig.java
+++ src/main/java/com/bolsedeideasspringboot/jpa/app/MvcConfig.java
@@ -1,15 +1,20 @@
 package com.bolsedeideasspringboot.jpa.app;
 
 import java.nio.file.Paths;
+import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -31,6 +36,28 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// creando Bean Idioma
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es","ES"));
+		return localeResolver;
+	}
+	
+	// Crear interceptor para idioma
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	// registrar interceptor
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 	
 }
